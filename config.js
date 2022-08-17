66720c557df9f862bde0c0a34fb5ffe407a34970
zins-mdd <111218997+zins-mdd@users.noreply.github.com>
2022-08-17 07:30:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
   var sticker_name = '@ZINS STORE REAL'
-  var sticker_author = 'aine'
+  var sticker_author = 'zinsbotz'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
